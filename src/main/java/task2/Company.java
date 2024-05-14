67633400d0a220a7045df951092528ab6a9effd6
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-14 17:12:00 +0300

    task2_refactored

--- src/main/java/task2/Company.java
+++ src/main/java/task2/Company.java
@@ -1,13 +1,36 @@
 package task2;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Company {
     private List<Employee> employees;
+    private Map<Employee, Double> salaries;
+
     // Конструктор та інші методи класу...
-    public void printEmployeeSalaries() {
+
+    public Company(List<Employee> employees) {
+        this.employees = employees;
+        this.salaries = new HashMap<>();
         for (Employee employee : employees) {
-            System.out.println(employee.getName() + ": " + employee.getSalary());
+            this.salaries.put(employee, 0.0);
+        }
+    }
+
+    public void setSalary(Employee employee, double salary) {
+        this.salaries.put(employee, salary);
+    }
+
+    public double getSalary(Employee employee) {
+        return this.salaries.get(employee);
+    }
+
+    public void printEmployeeSalaries() {
+        for (Map.Entry<Employee, Double> entry : salaries.entrySet()) {
+            Employee employee = entry.getKey();
+            double salary = entry.getValue();
+            System.out.println(employee.getName() + ": " + salary);
         }
     }
 }
--- src/main/java/task2/Employee.java
+++ src/main/java/task2/Employee.java
@@ -2,9 +2,8 @@ package task2;
 
 public class Employee {
     private String name;
-    private double salary;
     // Конструктор та інші методи класу...
-    public double getSalary() {
-        return salary;
+    public String getName() {
+        return name;
     }
 }
