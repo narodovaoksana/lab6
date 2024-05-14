835b093dcce5c8c2db4fb02e6e7327bf2a79d67c
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-14 17:01:37 +0300

    task1_refactored

--- src/main/java/task1/Bank.java
+++ src/main/java/task1/Bank.java
@@ -1,12 +1,21 @@
 package task1;
+
 import java.util.List;
 
 public class Bank {
     private List<Account> accounts;
     // Конструктор та інші методи класу...
+
     public void processAccounts() {
         for (Account account : accounts) {
-            account.calculateInterest();
+            double interest = calculateInterestForAccount(account);
+            System.out.println("Interest calculated: " + interest);
         }
+    }
+
+    private double calculateInterestForAccount(Account account) {
+        double balance = account.getBalance();
+        double interestRate = account.getInterestRate();
+        return balance * interestRate / 100;
     }
 }
