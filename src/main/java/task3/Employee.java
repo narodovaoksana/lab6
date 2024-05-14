c5ba7743a90fb05174d2d81fd1b79da427fb381e
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-14 18:33:16 +0300

    task3

---
+++ src/main/java/task3/Employee.java
@@ -0,0 +1,14 @@
+package task3;
+
+public class Employee {
+    private String name;
+    private String department;
+    private double salary;
+    // Конструктор та інші методи класу...
+    public void printEmployeeDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Salary: " + salary);
+    }
+    // Інші методи класу...
+}
