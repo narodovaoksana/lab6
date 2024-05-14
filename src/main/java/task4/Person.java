4f8178202058afac951ac5df620cef58fdf1ec6e
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-14 20:07:13 +0300

    task4_refactored

--- src/main/java/task4/Person.java
+++ src/main/java/task4/Person.java
@@ -2,10 +2,54 @@ package task4;
 
 public class Person {
     private String name;
-    private Address address;
-    // Конструктор, геттери та сеттери...
+    private String street;
+    private String city;
+    private String country;
+
+    public Person() {
+
+    }
+
+    public Person(String name, String street, String city, String country) {
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
     public void printAddress() {
-        System.out.println("Address: " + address.getStreet() + ", " + address.getCity
-                () + ", " + address.getCountry());
+        System.out.println("Address: " + street + ", " + city + ", " + country);
     }
 }
