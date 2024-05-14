a654bef34aedbf5b0634cc9b9395a26ab4cb32ce
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-14 20:09:08 +0300

    task5

---
+++ src/main/java/task5/Department.java
@@ -0,0 +1,10 @@
+package task5;
+
+public class Department {
+    private String name;
+    private Employee manager;
+    // Конструктор та інші методи класу...
+    public Employee getManager() {
+        return manager;
+    }
+}
---
+++ src/main/java/task5/Employee.java
@@ -0,0 +1,11 @@
+package task5;
+
+public class Employee {
+    private String name;
+    private Department department;
+    // Конструктор та інші методи класу...
+    public Department getDepartment() {
+        return department;
+    }
+}
+
