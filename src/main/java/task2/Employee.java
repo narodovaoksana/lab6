d044a59607fc1d90a19053b3e3a0c96356e23f2b
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-14 17:05:03 +0300

    task2

---
+++ src/main/java/task2/Employee.java
@@ -0,0 +1,10 @@
+package task2;
+
+public class Employee {
+    private String name;
+    private double salary;
+    // Конструктор та інші методи класу...
+    public double getSalary() {
+        return salary;
+    }
+}
