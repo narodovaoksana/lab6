37dbe6bb89db9a80bbc198ac1269664b4e262c6c
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-31 13:39:23 +0300

    task8

---
+++ src/main/java/part1/task8/DateHelper.java
@@ -0,0 +1,11 @@
+package part1.task8;
+
+import java.util.Date;
+
+public class DateHelper {
+    // Даний клас надає допоміжні функції пов'язані з роботою з датами.
+    public static Date addDays(Date date, int days) {
+        // Логіка додавання днів до дати.
+    }
+    // Інші методи класу...
+}
