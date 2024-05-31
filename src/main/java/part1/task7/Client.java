6fc5c3fa3125ec1f938cc2dbe565a83b7ec3c9f4
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-31 13:34:01 +0300

    task7 refactored

--- src/main/java/part1/task7/Client.java
+++ src/main/java/part1/task7/Client.java
@@ -1,13 +1,17 @@
 package part1.task7;
 import java.util.Calendar;
 public class Client {
-    // Інші методи класу...
     public void doSomething() {
         Calendar currentDate = Calendar.getInstance();
-        if (DateUtils.isWeekend(currentDate)) {
+        if (this.isWeekend(currentDate)) {
             System.out.println("It's weekend!");
         } else {
             System.out.println("It's not weekend!");
         }
+    }
+
+    public boolean isWeekend(Calendar date) {
+        int dayOfWeek = date.get(Calendar.DAY_OF_WEEK);
+        return dayOfWeek == Calendar.SATURDAY || dayOfWeek == Calendar.SUNDAY;
     }
 }
