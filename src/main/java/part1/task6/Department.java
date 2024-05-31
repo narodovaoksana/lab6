e69c418a49761eb21dae45d12b83b2d7d03b0ecc
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-31 13:19:19 +0300

    task6 refactored

--- src/main/java/part1/task6/Department.java
+++ src/main/java/part1/task6/Department.java
@@ -3,7 +3,13 @@ package part1.task6;
 public class Department {
     private String name;
     private Employee manager;
+
     // Конструктор та інші методи класу...
+    public Department(String name, Employee manager) {
+        this.name = name;
+        this.manager = manager;
+    }
+
     public String getManagerName() {
         return manager.getName();
     }
--- src/main/java/part1/task6/Employee.java
+++ src/main/java/part1/task6/Employee.java
@@ -3,8 +3,25 @@ package part1.task6;
 public class Employee {
     private String name;
     private Department department;
-    // Конструктор та інші методи класу...
-    public String getDepartmentManagerName() {
-        return department.getManagerName();
+
+    public Employee(String name, Department department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 }
