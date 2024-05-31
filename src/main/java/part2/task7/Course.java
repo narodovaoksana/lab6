5a184576cee45a38426f671ec94f9e6159670f10
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-31 14:01:20 +0300

    task2.7

---
+++ src/main/java/part2/task7/Course.java
@@ -0,0 +1,9 @@
+package part2.task7;
+
+public class Course {
+    private String title;
+    public Course(String title) {
+        this.title = title;
+    }
+    // Додаткові методи...
+}
---
+++ src/main/java/part2/task7/Student.java
@@ -0,0 +1,16 @@
+package part2.task7;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Student {
+    private String name;
+    private List<Course> courses;
+    public Student(String name) {
+        this.name = name;
+        this.courses = new ArrayList<>();
+    }
+    public void enrollCourse(Course course) {
+        courses.add(course);
+    }
+    // Додаткові методи...
+}
