47c65d35435cb6b62cf9d975b247547ec348c0f0
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-31 13:59:11 +0300

    task2.6

---
+++ src/main/java/part2/task5/OrderManager.java
@@ -0,0 +1,8 @@
+package part2.task5;
+
+public class OrderManager {
+    private String customerName;
+    private String productName;
+    private int quantity;
+    // Конструктор, геттери і сеттери, інші методи класу...
+}
