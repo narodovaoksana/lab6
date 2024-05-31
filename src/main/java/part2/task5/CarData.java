efcaee95f36bfa6d876d18547d510a1701c17689
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-05-31 13:57:49 +0300

    task2.4

---
+++ src/main/java/part2/task5/CarData.java
@@ -0,0 +1,13 @@
+package part2.task5;
+
+public class CarData {
+    private String[] carNames;
+    private int[] carPrices;
+    private int[] carYears;
+    public CarData(String[] carNames, int[] carPrices, int[] carYears) {
+        this.carNames = carNames;
+        this.carPrices = carPrices;
+        this.carYears = carYears;
+    }
+    // Додаткові методи для роботи з масивом...
+}
