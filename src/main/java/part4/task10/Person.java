a824fc13a1741bc55d64f9d957068b26df86fd7d
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 16:34:40 +0300

    task4.10

---
+++ src/main/java/part4/task10/Person.java
@@ -0,0 +1,15 @@
+package part4.task10;
+
+public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    // Інші методи класу
+}
+
