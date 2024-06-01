1c13a2e46c4160e9da3241f4bb3c81b225f10194
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 11:58:38 +0300

    task3.4

--- src/main/java/part3/task4/SearchElement.java
+++ src/main/java/part3/task4/SearchElement.java
@@ -1,4 +1,18 @@
 package part3.task4;
 
 public class SearchElement {
+    public void searchElement(int[] array, int target) {
+        boolean found = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+    }
 }
