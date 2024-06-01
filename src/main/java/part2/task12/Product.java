e106e32a25e149210e35920030df2b5a259b0152
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 11:07:52 +0300

    task2.12

---
+++ src/main/java/part2/task12/Product.java
@@ -0,0 +1,11 @@
+package part2.task12;
+
+public class Product {
+    private int typeCode;
+    private String name;
+    public Product(int typeCode, String name) {
+        this.typeCode = typeCode;
+        this.name = name;
+    }
+    // Додаткові методи...
+}
