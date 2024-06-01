cd8f59527231e07afd3b2303903dd96303c0b1dc
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 16:34:28 +0300

    task4.9

---
+++ src/main/java/part4/task9/User.java
@@ -0,0 +1,9 @@
+package part4.task9;
+
+public class User {
+    private String name;
+    private int age;
+    private String email;
+    // конструктор, геттери і сеттери
+}
+
---
+++ src/main/java/part4/task9/UserManager.java
@@ -0,0 +1,7 @@
+package part4.task9;
+
+public class UserManager {
+    public void createUser(String name, int age, String email) {
+        // створення об’єкту користувача і виконання дій
+    }
+}
