d86eb99b5f8a4a17f44d20d78821261abd87bf31
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 11:23:14 +0300

    task3.2

---
+++ src/main/java/part3/task2/Payment.java
@@ -0,0 +1,14 @@
+package part3.task2;
+
+public class Payment {
+    public void processPayment(double amount, boolean isMember, boolean
+            isDiscountAvailable) {
+        if (amount > 100 && isMember) {
+            applyDiscount();
+        }
+        if (amount > 200 && isDiscountAvailable) {
+            applyDiscount();
+        }
+    }
+
+}
