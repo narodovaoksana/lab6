a92ce07886bc5fcf23fbca07eed4f77c28eec269
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 01:50:06 +0300

    task 2.11 refactored

--- src/main/java/part2/task11/Library.java
+++ src/main/java/part2/task11/Library.java
@@ -2,15 +2,25 @@ package part2.task11;
 
 import java.awt.print.Book;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Library {
     private List<Book> books;
-    public Library(List<Book> books) {
-        this.books = books;
+
+    public Library() {
+        this.books = new ArrayList<>();
     }
+
     public List<Book> getBooks() {
-        return books;
+        return Collections.unmodifiableList(books);
     }
-    public void setBooks(List<Book> books) {
-        this.books = books;
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
     }
 }
