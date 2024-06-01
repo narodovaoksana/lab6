eec66b71b03f4205bca6fc46bc0cede111ad5dc4
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 16:31:54 +0300

    task4.7

---
+++ src/main/java/part4/task7/Person.java
@@ -0,0 +1,9 @@
+package part4.task7;
+
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+    // конструктор, геттери і сеттери
+}
+
---
+++ src/main/java/part4/task7/ProcessPerson.java
@@ -0,0 +1,7 @@
+package part4.task7;
+
+public class ProcessPerson {
+    public void process(String name, int age, String address) {
+        // обробка об'єкта Person
+    }
+}
