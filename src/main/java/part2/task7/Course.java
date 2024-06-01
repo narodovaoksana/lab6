e6ee875752aaa3b02aa36f33def1b8daa6bbf9da
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 01:32:50 +0300

    task 2.7 refactored

--- src/main/java/part2/task7/Course.java
+++ src/main/java/part2/task7/Course.java
@@ -1,9 +1,20 @@
 package part2.task7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
     private String title;
+    private List<Student> students;
+
     public Course(String title) {
         this.title = title;
+        this.students = new ArrayList<>();
     }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
     // Додаткові методи...
 }
--- src/main/java/part2/task7/Student.java
+++ src/main/java/part2/task7/Student.java
@@ -5,12 +5,16 @@ import java.util.List;
 public class Student {
     private String name;
     private List<Course> courses;
+
     public Student(String name) {
         this.name = name;
         this.courses = new ArrayList<>();
     }
+
     public void enrollCourse(Course course) {
         courses.add(course);
+        course.addStudent(this); // додавання студента до курсу
     }
+
     // Додаткові методи...
 }
