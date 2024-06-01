a00d79affb51c05c14014e64f3e2898d1642f4cf
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 02:50:30 +0300

    task 3.5 refactored

--- src/main/java/part3/task5/processOrder.java
+++ src/main/java/part3/task5/processOrder.java
@@ -2,15 +2,15 @@ package part3.task5;
 
 public class processOrder {
     public void processOrder(int quantity, double price) {
-        if (quantity > 0) {
-            if (price > 0) {
-                // Основна логіка
-                System.out.println("Order processed successfully");
-            } else {
-                System.out.println("Invalid price");
-            }
-        } else {
+        if (quantity <= 0) {
             System.out.println("Invalid quantity");
+            return;
         }
+        if (price <= 0) {
+            System.out.println("Invalid price");
+            return;
+        }
+
+        System.out.println("Order processed successfully");
     }
 }
