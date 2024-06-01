f7cda7f63a5e80919d2a2718df6e0b32260379e0
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 18:58:18 +0300

    task 2.3 refactored

--- src/main/java/part2/task3/Currency.java
+++ src/main/java/part2/task3/Currency.java
@@ -2,10 +2,11 @@ package part2.task3;
 
 public class Currency {
     private String code;
-    // Конструктор та інші методи класу...
+
     public Currency(String code) {
         this.code = code;
     }
+
     public String getCode() {
         return code;
     }
--- src/main/java/part2/task3/Product.java
+++ src/main/java/part2/task3/Product.java
@@ -4,20 +4,30 @@ public class Product {
     private String name;
     private double price;
     private Currency currency;
-    // Конструктор та інші методи класу...
+
+    // Створимо статичний метод для отримання екземпляра валюти
+    public static Currency getCurrency(String code) {
+        // Можна реалізувати кешування екземплярів Currency для покращення продуктивності
+        return new Currency(code);
+    }
+
     public Product(String name, double price, String currencyCode) {
         this.name = name;
         this.price = price;
-        this.currency = new Currency(currencyCode); // Кожний продукт має свійекземпляр валюти
+        this.currency = getCurrency(currencyCode); // Тепер ми використовуємо один екземпляр валюти для всіх продуктів з однаковою валютою
     }
+
     public String getName() {
         return name;
     }
+
     public double getPrice() {
         return price;
     }
+
     public String getCurrencyCode() {
         return currency.getCode();
     }
+
     // Інші методи класу...
 }
