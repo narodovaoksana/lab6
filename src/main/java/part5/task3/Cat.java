1f0dd2f131acaff25ac100b72e006827f37c80e5
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 17:51:38 +0300

    task 5.3

---
+++ src/main/java/part5/task3/Cat.java
@@ -0,0 +1,9 @@
+package part5.task3;
+
+class Cat extends Animal {
+    String color;
+    public Cat(String name, int age, String color) {
+        super(name, age);
+        this.color = color;
+    }
+}
---
+++ src/main/java/part5/task3/Dog.java
@@ -0,0 +1,10 @@
+package part5.task3;
+
+public class Dog extends Animal {
+    String breed;
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+}
+
