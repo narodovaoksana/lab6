d4c2a521e4b8620cac9b4b7716b0302cd12e0a86
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 13:22:49 +0300

    task3.7

---
+++ src/main/java/part3/task7/Address.java
@@ -0,0 +1,7 @@
+package part3.task7;
+
+public class Address {
+    private String street;
+    private String city;
+    // Конструктор, геттери та інші методи
+}
---
+++ src/main/java/part3/task7/Customer.java
@@ -0,0 +1,11 @@
+package part3.task7;
+
+public class Customer {
+    private Address address;
+    public Customer(Address address) {
+        this.address = address;
+    }
+    public Address getAddress() {
+        return address;
+    }
+}
