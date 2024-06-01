a45471484e9f06c87a8ee386e1885ad17c04b5de
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 01:24:23 +0300

    task 2.6 refactored

---
+++ src/main/java/part2/task6/Order.java
@@ -0,0 +1,8 @@
+package part2.task6;
+
+public class Order {
+    private String customerName;
+    private String productName;
+    private int quantity;
+    // Конструктор, геттери і сеттери, інші методи класу...
+}
--- src/main/java/part2/task6/OrderManager.java
+++ src/main/java/part2/task6/OrderManager.java
@@ -1,8 +1,15 @@
 package part2.task6;
 
+import java.util.List;
+
 public class OrderManager {
-    private String customerName;
-    private String productName;
-    private int quantity;
+    private List<Order> orders;
     // Конструктор, геттери і сеттери, інші методи класу...
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+    public void removeOrder(Order order) {
+        orders.remove(order);
+    }
+    // Інші методи для управління замовленнями...
 }
