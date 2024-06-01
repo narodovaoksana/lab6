ecef561d68d9699ef09b6282a742b7216258d737
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 16:06:11 +0300

    task3.8

--- src/main/java/part3/task8/calculateAverage.java
+++ src/main/java/part3/task8/calculateAverage.java
@@ -1,6 +1,6 @@
 package part3.task8;
 
-public class calculateAverage {
+public class CalculateAverage {
     public double calculateAverage(int[] numbers) {
         int sum = 0;
         for (int number : numbers) {
