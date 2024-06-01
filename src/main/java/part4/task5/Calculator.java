c89fb56b1d0b964812ec8c61984270a07e1c2125
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 16:29:01 +0300

    task4.5

---
+++ src/main/java/part4/task5/Calculator.java
@@ -0,0 +1,10 @@
+package part4.task5;
+
+public class Calculator {
+    public int calculateSquare(int num) {
+        return num * num;
+    }
+    public int calculateCube(int num) {
+        return num * num * num;
+    }
+}
