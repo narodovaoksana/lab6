61e5ce715b79b345f46247255f8030b07001d468
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 01:38:48 +0300

    task 2.8 refactored

--- src/main/java/part2/task8/Department.java
+++ src/main/java/part2/task8/Department.java
@@ -1,12 +1,15 @@
 package part2.task8;
 
 import java.util.List;
+
 public class Department {
     private String name;
     private List<Employee> employees;
+
     public Department(String name, List<Employee> employees) {
         this.name = name;
         this.employees = employees;
     }
+
     // Додаткові методи...
 }
--- src/main/java/part2/task8/Employee.java
+++ src/main/java/part2/task8/Employee.java
@@ -2,10 +2,10 @@ package part2.task8;
 
 public class Employee {
     private String name;
-    private Department department;
-    public Employee(String name, Department department) {
+
+    public Employee(String name) {
         this.name = name;
-        this.department = department;
     }
+
     // Додаткові методи...
 }
