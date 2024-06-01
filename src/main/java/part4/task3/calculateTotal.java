a623f82287145b8922caae348afdeb154133c46f
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 16:27:01 +0300

    task4.3

---
+++ src/main/java/part4/task3/calculateTotal.java
@@ -0,0 +1,8 @@
+package part4.task3;
+
+public class calculateTotal {
+    public double calculateTotal(double price, int quantity, double discount) {
+        return price * quantity * (1 - discount);
+    }
+
+}
