db2475a519e5684b16f0f29a51ce785f1392e353
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 11:03:00 +0300

    task2.8

---
+++ src/main/java/part2/task8/Employee.java
@@ -0,0 +1,11 @@
+package part2.task8;
+
+public class Employee {
+    private String name;
+    private Department department;
+    public Employee(String name, Department department) {
+        this.name = name;
+        this.department = department;
+    }
+    // Додаткові методи...
+}
