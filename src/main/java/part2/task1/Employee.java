0e2c533c0f3a7c4e28fb288a7fad2724d1c15a89
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-01 18:51:37 +0300

    task 2.1 refactored

--- src/main/java/part2/task1/Employee.java
+++ src/main/java/part2/task1/Employee.java
@@ -3,14 +3,19 @@ package part2.task1;
 public class Employee {
     private String name;
     private double salary;
-    // Конструктор та інші методи класу...
-    public void raiseSalary(double amount) {
-        if (amount > 0) {
-            salary += amount;
-        }
+
+    public void setSalary(double newSalary) {
+        salary = newSalary;
     }
+
     public double getSalary() {
         return salary;
+    }
+
+    public void raiseSalary(double amount) {
+        if (amount > 0) {
+            setSalary(getSalary() + amount);
+        }
     }
     // Інші методи класу...
 }
