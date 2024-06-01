b377062012df386b70bb9b33a7c639325d8b8614
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 01:45:13 +0300

    task 2.9 refactored

--- src/main/java/part2/task9/Example.java
+++ src/main/java/part2/task9/Example.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Example {
+    public static final int MAX_ELEMENT_COUNT = 100; // символічна константа
+
     public static void main(String[] args) {
         List<String> items = new ArrayList<>();
         // Додавання елементів у список
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < MAX_ELEMENT_COUNT; i++) {
             items.add("Item " + i);
         }
     }
 }
-
