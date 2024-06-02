83206b488239669cf3c13b367248a1282b559f36
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 03:26:58 +0300

    task 3.8 refactored

--- src/main/java/part3/task8/calculateAverage.java
+++ src/main/java/part3/task8/calculateAverage.java
@@ -1,7 +1,10 @@
 package part3.task8;
 
-public class CalculateAverage {
+public class calculateAverage {
     public double calculateAverage(int[] numbers) {
+        assert numbers!= null : "The numbers array cannot be null";
+        assert numbers.length > 0 : "The numbers array cannot be empty";
+
         int sum = 0;
         for (int number : numbers) {
             sum += number;
