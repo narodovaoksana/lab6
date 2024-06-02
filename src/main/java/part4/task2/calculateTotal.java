3e74533f7b167f725365b158b1a33442559faf23
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 03:30:35 +0300

    task 4.2 refactored

--- src/main/java/part4/task2/calculateTotal.java
+++ src/main/java/part4/task2/calculateTotal.java
@@ -1,7 +1,8 @@
 package part4.task2;
 
 public class calculateTotal {
-    public double calculateTotal(double price, int quantity) {
-        return price * quantity;
+    public double calculateTotal(double price, int quantity, double taxRate) {
+        double subtotal = price * quantity;
+        return subtotal + (subtotal * taxRate);
     }
 }
