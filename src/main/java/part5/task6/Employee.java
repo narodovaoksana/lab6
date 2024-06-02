deaa9881a88e3ff47f9429a461c20146f545aada
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 04:27:43 +0300

    task 5.6 refactored

--- src/main/java/part5/task6/Employee.java
+++ src/main/java/part5/task6/Employee.java
@@ -2,20 +2,30 @@ package part5.task6;
 
 public class Employee {
     private String name;
-    private int salary;
-    private int bonus;
 
-    public Employee(String name, int salary, int bonus) {
+    public Employee(String name) {
         this.name = name;
-        this.salary = salary;
-        this.bonus = bonus;
     }
 
     public void displayInfo() {
         System.out.println("Name: " + name);
+    }
+}
+
+class Manager extends Employee {
+    private int salary;
+    private int bonus;
+
+    public Manager(String name, int salary, int bonus) {
+        super(name);
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
         System.out.println("Salary: " + salary);
-        if (bonus!= 0) {
-            System.out.println("Bonus: " + bonus);
-        }
+        System.out.println("Bonus: " + bonus);
     }
 }
