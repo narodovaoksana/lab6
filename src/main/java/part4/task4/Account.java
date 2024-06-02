1bb6f409f6f4c18fa900fae6f0d46a5f1696326a
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 03:33:17 +0300

    task 4.4 refactored

--- src/main/java/part4/task4/Account.java
+++ src/main/java/part4/task4/Account.java
@@ -2,9 +2,13 @@ package part4.task4;
 
 public class Account {
     private double balance;
+
     public double getBalance() {
+        return balance;
+    }
+
+    public void deductCommission() {
         double commission = 0.05;
         balance -= balance * commission;
-        return balance;
     }
 }
