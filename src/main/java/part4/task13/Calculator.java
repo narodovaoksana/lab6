7c52fb0fdb80d4cfd26725261ad62236bec29526
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 04:03:59 +0300

    task 4.13 refactored

--- src/main/java/part4/task13/Calculator.java
+++ src/main/java/part4/task13/Calculator.java
@@ -2,19 +2,18 @@ package part4.task13;
 
 public class Calculator {
     public static int divide(int dividend, int divisor) {
-
         if (divisor == 0) {
-            return -1; // Повертаємо -1 у випадку ділення на нуль
+            throw new ArithmeticException("Cannot divide by zero");
         }
         return dividend / divisor;
     }
 
     public static void main(String[] args) {
-        int result = divide(10, 0);
-        if (result == -1) {
-            System.out.println("Помилка: Ділення на нуль");
-        } else {
-            System.out.println("Результат ділення: " + result);
+        try {
+            int result = divide(10, 0);
+            System.out.println("The result of division: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
