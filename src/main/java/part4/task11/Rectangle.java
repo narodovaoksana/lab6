87129aa696f94d14e5979a261cad66627b6f3fe0
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 03:53:27 +0300

    task 4.11

---
+++ src/main/java/part4/task11/Rectangle.java
@@ -0,0 +1,23 @@
+package part4.task11;
+
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int calculateArea() {
+        return width * height;
+    }
+}
