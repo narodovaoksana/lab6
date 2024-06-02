4883af333347046cc65cae254fb4efdafbb07821
narodovaoksana <narodova.oksana@chnu.edu.ua>
2024-06-02 03:36:20 +0300

    task 4.6 refactored

--- src/main/java/part4/task6/Printer.java
+++ src/main/java/part4/task6/Printer.java
@@ -1,11 +1,19 @@
 package part4.task6;
 
 public class Printer {
+    public void printTextDocument(String content) {
+        System.out.println("Printing text document: " + content);
+    }
+
+    public void printImageDocument(String content) {
+        System.out.println("Printing image document: " + content);
+    }
+
     public void printDocument(String type, String content) {
         if ("text".equals(type)) {
-            System.out.println("Printing text document: " + content);
+            printTextDocument(content);
         } else if ("image".equals(type)) {
-            System.out.println("Printing image document: " + content);
+            printImageDocument(content);
         } else {
             System.out.println("Unknown document type: " + type);
         }
